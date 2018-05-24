dda7318496fedd504b1d9029ee913d9eca1a9375
laoruiwen <421365967@qq.com>
2018-05-24 08:25:01 +0800

    加上anmaze

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.use(express.static('dist'));
-var server = app.listen(8888, function () {
+var server = app.listen(11111, function () {
     var host = server.address().address;
     var port = server.address().port;
 
