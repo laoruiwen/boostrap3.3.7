b0c6a34b0b90b101f281689428c159216397915b
laoruiwen <421365967@qq.com>
2018-05-29 09:56:25 +0800

    加上anmaze

--- Gruntfile.js
+++ Gruntfile.js
@@ -108,17 +108,17 @@ module.exports = function (grunt) {
         src: [
           'js/transition.js',
           'js/alert.js',
-          'js/button.js',
-          'js/carousel.js',
+          //'js/button.js',
+          //'js/carousel.js',
           'js/collapse.js',
           'js/dropdown.js',
           'js/modal.js',
           'js/tooltip.js',
           'js/popover.js',
-          'js/scrollspy.js',
+          //'js/scrollspy.js',
           'js/tab.js',
-          'js/affix.js',
-          'js/bootstrap-switch.js',
+          //'js/affix.js',
+          //'js/bootstrap-switch.js',
           'js/radiocheck.js'
         ],
         dest: 'dist/js/<%= pkg.name %>.js'
